b73c54ef443b349601f0b63064d7018df413dbf3
James Howard and Wolfgang Warneke <james+wolfgang@>
2016-07-21 16:29:29 -0700

    add front-end functionality for listing books from all categories

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -17,6 +17,50 @@ namespace HomeLibrary
         }
         return View["index.cshtml", model];
       };
+      Get["/Display/Your"] = _ => {
+        List<Books> model = new List<Books> {};
+        foreach (var book in OwnedBooks.GetAll())
+        {
+          model.Add(Books.Find(book.GetBookId()));
+        }
+        return View["index.cshtml", model];
+      };
+      Get["/Display/Lent"] = _ => {
+        List<Books> model = new List<Books> {};
+        foreach (var book in LentBooks.GetAll())
+        {
+          int ownedId = book.GetOwnedBookId();
+          OwnedBooks ownedBook = OwnedBooks.Find(ownedId);
+          int allBookId = ownedBook.GetBookId();
+          Books bookToAddToModel = Books.Find(allBookId);
+          model.Add(bookToAddToModel);
+        }
+        return View["index.cshtml", model];
+      };
+      Get["/Display/Borrowed"] = _ => {
+        List<Books> model = new List<Books> {};
+        foreach (var book in BorrowedBooks.GetAll())
+        {
+          model.Add(Books.Find(book.GetBookId()));
+        }
+        return View["index.cshtml", model];
+      };
+      Get["/Display/Wishlist"] = _ => {
+        List<Books> model = new List<Books> {};
+        foreach (var book in Wishlist.GetAll())
+        {
+          model.Add(Books.Find(book.GetBookId()));
+        }
+        return View["index.cshtml", model];
+      };
+      Get["/Display/ToSell"] = _ => {
+        List<Books> model = new List<Books> {};
+        foreach (var book in BooksToSell.GetAll())
+        {
+          model.Add(Books.Find(book.GetBookId()));
+        }
+        return View["index.cshtml", model];
+      };
     }
   }
 }
